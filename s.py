1b115d23debda1779b0d27b2e77a90b3fb354682
sainipriya002 <101283945+sainipriya002@users.noreply.github.com>
2022-03-16 10:50:25 +0530

    Add files via upload
    from PIL extraction of exif data.

---
+++ s.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+#this is a test for our live video on fB
+# open test
+def get_exif(filename):
+    image = Image.open(filename)
+    image.verify()
+    return image.getexif()
+
+# extract the exif data from image file
+exif = get_exif('filename') # included image
+
+# Loop through tags for print out
+if exif:
+    for k, v in exif.items():
+        print(str([TAGS.get(k)]) + "\t" + str(v)) 
